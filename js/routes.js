73b3325fe798d5aabec6e964d21e3d9fad5b9253
zis76 <76632633+zis76@users.noreply.github.com>
2026-04-02 16:12:51 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -3,7 +3,6 @@ import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js';
 import PlayerComparison from './pages/PlayerComparison.js';
-import Illist from './pages/illist.js';
 
 export default [
     { path: '/', component: List },
@@ -12,5 +11,4 @@ export default [
     { path: '/listpacks', component: ListPacks },
     { path: '/player', component: PlayerComparison },
     { path: '/player-comparison', component: PlayerComparison },
-    { path: '/illist', component: illist },
 ];
