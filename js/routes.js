94b16eba2254b2b3364694121d8f15cc5c32b826
zis76 <76632633+zis76@users.noreply.github.com>
2026-04-02 01:50:45 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,7 +2,7 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js';
-import PlayerComparison from './pages/playercomp.js';
+import PlayerComparison from './pages/Playercomp.js';
 
 
 export default [
