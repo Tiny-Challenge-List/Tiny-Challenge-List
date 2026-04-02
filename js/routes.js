9d318d59932325a4e2585cd3aa09069331773ccb
zis76 <76632633+zis76@users.noreply.github.com>
2026-04-02 01:49:27 -0500

    Update routes.js

--- js/routes.js
+++ js/routes.js
@@ -2,10 +2,13 @@ import List from './pages/List.js';
 import Leaderboard from './pages/Leaderboard.js';
 import Roulette from './pages/Roulette.js';
 import ListPacks from './pages/ListPacks.js';
+import PlayerComparison from './pages/playercomp.js';
+
 
 export default [
     { path: '/', component: List },
     { path: '/leaderboard', component: Leaderboard },
     { path: '/roulette', component: Roulette },
     { path: '/listpacks', component: ListPacks },
+    { path: '/player', component: PlayerComparison },
 ];
