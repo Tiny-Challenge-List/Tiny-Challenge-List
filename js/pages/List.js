d5101c3f054fdf88085b9bd3c3adeb593786b922
RustyGDZIS <tinepollo@gmail.com>
2026-05-16 12:09:13 +1000

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -134,7 +134,7 @@ export default {
         <table class="list" v-if="filteredList.length">
           <tr v-for="(item, i) in filteredList" :key="i">
             <td class="rank">
-              <p v-if="getOriginalRank(item[0]) <= 200" class="type-label-lg">
+              <p v-if="getOriginalRank(item[0]) <= 150" class="type-label-lg">
                 #{{ getOriginalRank(item[0]) }}
               </p>
               <p v-else class="type-label-lg">Legacy</p>
@@ -199,7 +199,7 @@ export default {
           <p v-if="selectedIndexInFullList <= 75">
             <strong>{{ selectedLevel.percentToQualify }}%</strong> or better to qualify
           </p>
-          <p v-else-if="selectedIndexInFullList <= 200">
+          <p v-else-if="selectedIndexInFullList <= 150">
             <strong>100%</strong> or better to qualify
           </p>
           <p v-else>This level does not accept new records.</p>
